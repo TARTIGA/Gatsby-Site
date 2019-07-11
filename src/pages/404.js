ab6205b4fc2b986147f26b0cb2add1f9e06122fa
TARTIGA <timigntrt@gmail.com>
2019-07-11 18:25:57 +0300

    add filesystem plugins

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default () => (
   <div>
     <Layout>
-      <div>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-      </div>
+      <div>NO NO NO</div>
     </Layout>
   </div>
 )
