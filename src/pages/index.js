6c962a010e106f07937623c80f2761db1f490286
TARTIGA <timigntrt@gmail.com>
2019-07-13 17:32:52 +0300

    plugins for PWA and SEO, manifest,offline,helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import { Helmet } from "react-helmet" // for title desc in short SEO
 
 export default ({ data }) => {
   return (
     <Layout>
       <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title22</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <h1>Amazing Pandas Eating Things</h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
