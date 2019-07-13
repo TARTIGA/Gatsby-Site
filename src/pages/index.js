514911dbf5612de9927ab43cc58f1a070d7d76ef
TARTIGA <timigntrt@gmail.com>
2019-07-13 17:04:48 +0300

    add markdown blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
-  <div>
+export default ({ data }) => {
+  console.log(data)
+  return (
     <Layout>
       <div>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <h1>Amazing Pandas Eating Things</h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
       </div>
     </Layout>
-  </div>
-)
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
